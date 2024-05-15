2f041eff3d3079b9cebfdf2e7ea591a97cbc8451
Jaime Mejia-Beher <jmejiabeher@gmail.com>
2024-05-15 16:57:15 -0600

    ready for submission

---
+++ src/pages/Price.jsx
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+export default function Price(props) {
+
+  const apiKey = "4EA69C98-821F-4C8C-A88F-C892A560B6C8";
+  const params = useParams();
+  const symbol = params.symbol;
+  const url = `http://rest.coinapi.io/v1/exchangerate/${symbol}/USD?apikey=${apiKey}`;
+
+  const [coin, setCoin] = useState("null");
+
+  const getCoin = async () => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setCoin(data);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  useEffect(() => {
+    getCoin();
+  }, []);
+
+  const loaded = () => {
+    return (
+      <div>
+        <h1>
+          {coin.asset_id_base}/{coin.asset_id_quote}
+        </h1>
+        <h2>{coin.rate}</h2>
+      </div>
+    );
+  };
+
+  // Function for when data doesn't exist.
+  const loading = () => {
+    return <h1>Loading...</h1>;
+  };
+
+  // If coin has data, run the loaded function; otherwise, run loading.
+  return coin && coin.rate ? loaded() : loading();
+}
